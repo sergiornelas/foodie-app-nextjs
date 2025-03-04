672bf0bef1a8596fef7a0af0c90b8f303a331072
sergiornelas <sergio.ornelas.92@outlook.com>
2025-03-04 15:32:57 -0600

    slideshow ready

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,4 @@
+// localhost:3000/meals/{something}
+export default function MealsDetailsPage({ params }) {
+  return <h1>MealsPage</h1>;
+}
--- app/meals/[slug]/page.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function MealsPage({ params }) {
-  return <h1>MealsPage</h1>;
-}
