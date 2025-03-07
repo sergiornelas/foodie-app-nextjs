547d3d08f69217c8538797d03b30a6f251d0eb5b
sergiornelas <sergio.ornelas.92@outlook.com>
2025-03-07 03:11:28 -0600

    deleted redundant info

--- app/page.js
+++ app/page.js
@@ -33,19 +33,6 @@ export default function Home() {
             with other food lovers.
           </p>
         </section>
-
-        <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
-          <p>
-            NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes,
-            and to connect with other food lovers.
-          </p>
-          <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
-          </p>
-        </section>
       </main>
     </>
   );
